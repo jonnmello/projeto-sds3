b1f034e1c2444b85921e751a40e27097ad1e5b4f
Jonatas Melo <jonnmello@hotmail.com>
2021-11-05 16:24:45 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -33,15 +33,7 @@ const DonutChart = () => {
 
     }, []);
 
-    // forma errada
-    // let chartData : ChartData = {labels:[], series:[]};
-    // forma errada
-
-
-    //const mockData = {
-    //  series: [477138, 499928, 444867, 220426, 473088],
-    //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
+  
 
     const options = {
         legend: {
