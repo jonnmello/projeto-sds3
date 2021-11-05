53bb569f2d95195083de7ffe5bced99d290b7572
Jonatas Melo <jonnmello@hotmail.com>
2021-11-05 12:10:21 -0300

    Routes

---
+++ frontend/src/RoutesConfig.tsx
@@ -0,0 +1,24 @@
+import Dashboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const RoutesConfig = () => {
+    return (
+    
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            
+            <Route path="/dashboard"element={<Dashboard/>}/>
+           
+
+            
+
+        </Routes>
+        
+        </BrowserRouter>
+
+    );
+}
+
+export default RoutesConfig;
